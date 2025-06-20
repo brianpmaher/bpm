42d0231951e66833be16c22a5865a81279764d93
Brian Maher <bmaher91@gmail.com>
2025-06-20 07:12:30 -0600

    Nit: Cleanup some script logs

--- scripts/windows/_setup_logger.bat
+++ scripts/windows/_setup_logger.bat
@@ -18,9 +18,10 @@ set "blue=%esc%[34m"
 set "magenta=%esc%[35m"
 set "cyan=%esc%[36m"
 set "white=%esc%[37m"
+set "gray=%esc%[90m"
 set "reset=%esc%[0m"
 
-set "log_info=%white%[INFO]"
+set "log_info=%gray%[INFO]"
 set "log_warn=%yellow%[WARNING]"
 set "log_error=%red%[ERROR]"
 set "log_success=%green%[SUCCESS]"
@@ -28,7 +29,7 @@ set "log_end=%reset%"
 
 set _logger_initialized=1
 
-echo %log_info% Logger initialized %log_end%
+echo %log_info% Logger initialized. %log_end%
 
 :exit_success
 exit /b 0
--- scripts/windows/setup_msvc_env.bat
+++ scripts/windows/setup_msvc_env.bat
@@ -9,7 +9,10 @@ call "%~dp0_goto_project_root.bat"
 call scripts\windows\_setup_logger.bat
 
 if defined _msvc_env_initialized (
-    goto :exit_success
+    if not "%1"=="_standalone" (
+        echo %log_success% MSVC environment already initialized. %log_end%
+    )
+    goto exit_success
 )
 
 echo %log_info% Initializing MSVC environment... %log_end%
@@ -20,11 +23,13 @@ if %errorlevel% neq 0 (
     echo %log_error%  - Otherwise install Visual Studio with C++ components ^(https://visualstudio.microsoft.com/downloads/^) and try again. %log_end%
     goto exit_error
 )
-echo %log_info% MSVC environment initialized. %log_end%
 
 if "%1"=="_standalone" (
+    echo %log_info% MSVC environment initialized once. %log_end%
     echo %log_warn% Speed up your builds by calling `scripts\windows\setup_msvc_env.bat` in your terminal to setup the MSVC environment for the terminal session. %log_end%
     echo %log_warn% VSCode: Run this script in your terminal, launch `code` from the same terminal session to speed up builds in your editor. %log_end%
+) else (
+    echo %log_success% MSVC environment initialized in current terminal session. %log_end%
 )
 
 set _msvc_env_initialized=1
